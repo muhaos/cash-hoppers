57d8ca50f80c695cf82ddaf2a90a484fd7c50a92
Volodimir Musienko <volodymir.musienko@gmail.com>
2013-06-14 17:53:03 +0300

    Login form design

---
+++ CashHoppers/CashHoppers/CHLoginVC.h
@@ -0,0 +1,13 @@
+//
+//  CHLoginVC.h
+//  CashHoppers
+//
+//  Created by Vova Musiienko on 14.06.13.
+//  Copyright (c) 2013 swanteams.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHLoginVC : UIViewController
+
+@end
---
+++ CashHoppers/CashHoppers/CHLoginVC.m
@@ -0,0 +1,43 @@
+//
+//  CHLoginVC.m
+//  CashHoppers
+//
+//  Created by Vova Musiienko on 14.06.13.
+//  Copyright (c) 2013 swanteams.com. All rights reserved.
+//
+
+#import "CHLoginVC.h"
+
+@interface CHLoginVC ()
+
+@property (assign, nonatomic) BOOL oldNavBarStatus;
+
+@end
+
+@implementation CHLoginVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.oldNavBarStatus = self.navigationController.navigationBarHidden;
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+
+- (void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:self.oldNavBarStatus animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
