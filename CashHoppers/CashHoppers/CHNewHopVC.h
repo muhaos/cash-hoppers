e2f92d0b9d173b889a6c349594d46682ce20f80a
Eugene <jekahy@gmail.com>
2013-06-24 15:57:51 +0300

    add NewHopVC

---
+++ CashHoppers/CashHoppers/CHNewHopVC.h
@@ -0,0 +1,23 @@
+//
+//  CHNewHopVC.h
+//  CashHoppers
+//
+//  Created by Eugene on 24.06.13.
+//  Copyright (c) 2013 swanteams.com. All rights reserved.
+//
+#define CH_NUMBER_OF_CHARACTERS 140
+#define CH_GRAY_COLOR [UIColor colorWithWhite:.9 alpha:.8]
+
+#import <UIKit/UIKit.h>
+
+@interface CHNewHopVC : UIViewController <UITextViewDelegate,UIImagePickerControllerDelegate>
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) IBOutlet UILabel *charCountLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *photoImView;
+@property (strong, nonatomic) IBOutlet UIButton *submitButton;
+@property (strong, nonatomic) IBOutlet UIImageView *separatorView;
+- (IBAction)resignResponder:(id)sender;
+- (IBAction)photoTapped:(id)sender;
+- (IBAction)submitPressed:(id)sender;
+
+@end
