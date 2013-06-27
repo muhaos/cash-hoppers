87f44967dd8dc74da6ee440f809ec05cf507d2bf
Balazh Vasyl <bgwasja@gmail.com>
2013-06-27 19:00:01 +0300

    Add OtherHopsList view

---
+++ CashHoppers/CashHoppers/CHOtherHopsListCell.h
@@ -0,0 +1,39 @@
+//
+//  CHOtherHopsListCell.h
+//  CashHoppers
+//
+//  Created by Balazh Vasyl on 6/27/13.
+//  Copyright (c) 2013 swanteams.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHOtherHopsListCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *verticalIndicatorImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *horizontalIndicatorImageView;
+@property (strong, nonatomic) IBOutlet UILabel *nameHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *dateHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *namePrizeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *countPrizeLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *prewNameHopLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *prewVerticalIndicator;
+@property (strong, nonatomic) IBOutlet UILabel *prewDateHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *prewPrizeHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *prewCountHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *prewFeeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *prewCountFeeLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *joinNameHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *joinDateHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *joinPrizeHopLabel;
+@property (strong, nonatomic) IBOutlet UILabel *joinCountHopLabel;
+@property (strong, nonatomic) IBOutlet UIButton *joinButton;
+@property (strong, nonatomic) IBOutlet UIImageView *lockImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *joinVerticalIndicatorImageView;
+
+- (IBAction)previewButtonTapped:(id)sender;
+- (IBAction)joinButtonTapped:(id)sender;
+
+@end
