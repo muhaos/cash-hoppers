3c770f25ce061394e65ddd01d6b5124c2427988d
Balazh Vasyl <bgwasja@gmail.com>
2013-08-16 17:27:20 +0300

    fix navbar in reset password view

--- CashHoppers/CashHoppers/CHResetPasswordVC.m
+++ CashHoppers/CashHoppers/CHResetPasswordVC.m
@@ -34,11 +34,13 @@
 
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
 }
 
 
 - (void) viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
 }
 
 
