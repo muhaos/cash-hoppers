95db3a3b108ef6ee5e541ee48059d0987d9ceacf
Balazh Vasyl <bgwasja@gmail.com>
2013-09-10 19:08:10 +0300

    fix sharing with g+

--- CashHoppers/CashHoppers/CHNewHopVC.m
+++ CashHoppers/CashHoppers/CHNewHopVC.m
@@ -293,6 +293,7 @@
 
 
 - (void) adsClosedTapped {
+    [CHSharingPopupVC instance].imageToShareURL = self.currentHopTask.photoURL;
     [CHSharingPopupVC instance].hopTaskID = self.currentHopTask.identifier;
     [CHSharingPopupVC instance].imageToShare = self.photoImView.image;
     [[CHSharingPopupVC instance]showInController:self.parentViewController.parentViewController];
