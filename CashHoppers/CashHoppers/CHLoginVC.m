757ebe15468b4bbbc9ad81fb460d7e6ec989fb12
Balazh Vasyl <bgwasja@gmail.com>
2013-09-11 12:02:36 +0300

    fix alert when cant login

--- CashHoppers/CashHoppers/CHLoginVC.m
+++ CashHoppers/CashHoppers/CHLoginVC.m
@@ -184,8 +184,8 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
                 [self goToTabBar];
                 
             }else{
-              //  message = [NSString stringWithFormat:@"Login unsuccessfull: %@",[JSON objectForKey:@"errors"]];
-                message = [NSString stringWithFormat:@"Login failed - wrong email or password"];
+                message = [NSString stringWithFormat:@"Login unsuccessfull: %@",[JSON objectForKey:@"errors"]];
+              //  message = [NSString stringWithFormat:@"Login failed - wrong email or password"];
                 UIAlertView* av = [[UIAlertView alloc] initWithTitle:@"LOGIN" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
                 
                 [av show];
