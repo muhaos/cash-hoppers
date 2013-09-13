c7030b5f3c7a67b8f111824035e958902e7dd691
Volodimir Musienko <volodymir.musienko@gmail.com>
2013-09-13 10:27:39 +0300

    changed error msg for registration

--- CashHoppers/CashHoppers/CHRegisterVC.m
+++ CashHoppers/CashHoppers/CHRegisterVC.m
@@ -102,7 +102,7 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
     [params setObject:userNameTextField.text forKey:@"user_name"];
     [params setObject:@"image/jpeg" forKey:@"avatar_content_type"];
     [params setObject:@"myavatar.jpg" forKey:@"avatar_original_filename"];
-//    [params setObject:@"/9j/4AAQSkZJRgABAQAAAQABAAD//gA7Q1JFQVRPUjogZ2QtanBlZyB2MS4wICh1c2luZyBJSkcgSlBFRyB2ODApLCBxdWFsaXR5ID0gOTUK/9sAQwACAQEBAQECAQEBAgICAgIEAwICAgIFBAQDBAYFBgYGBQYGBgcJCAYHCQcGBggLCAkKCgoKCgYICwwLCgwJCgoK/9sAQwECAgICAgIFAwMFCgcGBwoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoK/8AAEQgAMgAyAwEiAAIRAQMRAf/EAB8AAAEFAQEBAQEBAAAAAAAAAAABAgMEBQYHCAkKC//EALUQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+v/EAB8BAAMBAQEBAQEBAQEAAAAAAAABAgMEBQYHCAkKC//EALURAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIjKBCBRCkaGxwQkjM1LwFWJy0QoWJDThJfEXGBkaJicoKSo1Njc4OTpDREVGR0hJSlNUVVZXWFlaY2RlZmdoaWpzdHV2d3h5eoKDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uLj5OXm5+jp6vLz9PX29/j5+v/aAAwDAQACEQMRAD8A/frjsP5Ucen8qM8fe/WjJz979aAA49KOPT+VGf8Aa7+tGR/eoAOPT+VHHp+ooyOPmoz/ALX60AGD2X+VFGV/vmigDnfi7ca1Z/C3xDf+Hddn0y/ttHuJ7TULaKJ3gkSNnVgsyOh5HRlIxmvMf2N/iV4u+JX7D/hr4w/GL4iXM+pa54YfUtY1s28FubUMrFmjWGNUVUUZGVJ45Jr0f44anYaL8GvFWpalcLDDH4fu9zucDJhYAD1JJAA6kkAV8qfC3xza+F/+CK8lrbSsNZsvhbe6OumbG+0pqT20scduYvviUs6EJjJBB6HNe7gcKsVlnKo6yr043srpSi769r622utT5vMcY8Jm/M5Pljh6suW7s3GStpfe10nvZ6F7xB+2N8TEsU0jTfEtouvXkreHoW+X7JH4qtB9rs4j6WesWm0xsD8mVCne5I639iH9tnVP2tfix4xkTT59O0GDwl4f1Lw/pN5a+XPA8/22O9DkgFyJ4dgb7pVFKj5iT43+1h+yHrHjzWfhs/wogubzw18SvDmn+EvGF3ouHOn3FlD9p0vW0IIw0BilRnyMxfuwcuK9P0SXw18HP+CoUnh8Tiz0fXfglYafFdSJthbUoNSlEUDOAEWZ4ZSwU4LdhyK9ythcnq5ZJUIJ1ZQnNd48koppru9bdbaq6bt87hsXnlHNYPETapRqU4PtL2kZuLTfRe7fo3o7NK/V/B3xn8WP2zLDVPir4Y+L2o+CfAq63eab4Ti8M2FlJe6rFazvbyXs817BOqI8scgSKONCFUFnbdgd38IPAXx18B+P9ctPH/xsvfGfhebT7VtAl1jTLKC9tbnfMLhJHtIYllXb5JVigxlhzjNeCfsofEI/sBabqv7Jv7SWl6jo/h3SNevrn4feOzp00ul6jptzcPcLDLPGrLb3EbySBll2ggjaSME7f7JWqab8Rv23PjD8Q/BWtaxqngtdK0NfDV+91dvpjXckc7X32bzD5RYssW/YOMjpnngx2CnB4n2SisPGN4Pli1KPNBRtPdTd7y9691NOKsrenl+OpS+qKs5PEyk1UjzyTjLlm5XhezgrWjaNrODUnd3+qfn9qKOPf9aK+VPsxCARgkflSeWv90flS/h/Kg/7tAAABwMflRsUnOBn6UHHpRx/nFAAUDdQD+FIEVRkAflS8f5xRxjp/KgB3PqPyopvH+cUUAICSDk0pJ55oooAMnPX/OaXJ9f85oooAATkc0mTjr/nFFFADqKKKAP/2Q==" forKey:@"base64avatar"];
+
     [params setObject:image64string forKey:@"base64avatar"];
     [params setObject:@"myavatar.jpg" forKey:@"avatar_original_filename"];
     [params setObject:zipTextField.text/*@"123456789"*/ forKey:@"zip"];
@@ -151,7 +151,7 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
             } else {
                 errMsg = [error localizedDescription];
             }
-            UIAlertView* av = [[UIAlertView alloc] initWithTitle:@"REGISTRATION" message:[NSString stringWithFormat:@"Registration unsuccessful : %@", errMsg] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+            UIAlertView* av = [[UIAlertView alloc] initWithTitle:@"REGISTRATION" message:[NSString stringWithFormat:@"Registration unsuccessful. Try again later."] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
             [av show];
             [[CHLoadingVC sharedLoadingVC] hide];
         }];
